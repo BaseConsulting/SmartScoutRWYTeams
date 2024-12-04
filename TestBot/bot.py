ca937673d7586a6cf280544b977ce5f7db435ebd
jpapouse <jan.papousek@base.cz>
2024-12-04 14:43:27 +0000

    bot OAuth

--- TestBot/bot.py
+++ TestBot/bot.py
@@ -3,12 +3,25 @@
 
 from botbuilder.core import ActivityHandler, TurnContext
 from botbuilder.schema import ChannelAccount
+from botbuilder.core import CardFactory, MessageFactory
+from botbuilder.schema import SigninCard, ActionTypes
 
 
 class MyBot(ActivityHandler):
     # See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
 
     async def on_message_activity(self, turn_context: TurnContext):
+        card = SigninCard(
+        text="Please sign in to continue",
+        buttons=[
+            {
+                "type": ActionTypes.signin,
+                "title": "Sign in",
+                "value": "https://login.microsoftonline.com/"
+            }
+        ]
+        )
+        await turn_context.send_activity(MessageFactory.attachment(CardFactory.signin_card(card)))
         await turn_context.send_activity(f"You said '{ turn_context.activity.text }'")
 
     async def on_members_added_activity(
